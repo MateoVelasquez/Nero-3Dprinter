797550fdfae25b9b0b3d48d6fe9dfb5ee3b73db9
MateoVelasquez <mateo10velasquez@hotmail.com>
2025-07-09 00:19:45 -0500

    Define metadata ID and name of machine

--- Firmware/MarlinFirmware/Marlin/Configuration.h
+++ Firmware/MarlinFirmware/Marlin/Configuration.h
@@ -61,7 +61,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Mateo Velásquez (NeroPrusa Config v1.0)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -138,11 +138,11 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-//#define CUSTOM_MACHINE_NAME "3D Printer"
+#define CUSTOM_MACHINE_NAME "NeroPrusa MVM"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "4488dbce-747b-4757-907f-d765f894fe94"
 
 // @section stepper drivers
 
